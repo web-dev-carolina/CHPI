a5a6bb816b70cc63f9cd16811761ddcc2655905b
plector <kenanpoole@gmail.com>
2020-10-11 20:57:04 -0400

    Starting Structure

--- chpi-website/src/App.js
+++ chpi-website/src/App.js
@@ -2,32 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Filler from "./Component/filler.js";
+import Navbar from "./Component/Navbar.js"
 
 function App() {
   return (
 
     <div>
 
-      <div>
-        <Filler />
+      <div>.
+        <Navbar />
       </div>
 
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
 
     </div>
 
---
+++ chpi-website/src/Component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../CSS/Navbar.css"
+
+function Navbar() {
+    return(
+
+        <div className="Navbar-container">
+            <h1> Hello</h1>
+
+        </div>
+    )
+
+
+}
+
+export default Navbar;
