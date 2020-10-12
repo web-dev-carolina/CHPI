8310b70d91407b40e96575c114c54c2e73edc7fd
plector <kenanpoole@gmail.com>
2020-10-11 20:44:42 -0400

    Filler Example

---
+++ chpi-website/src/Component/filler.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../CSS/filler.css"
+
+function filler() {
+    return(
+
+        <div className="filler-container">
+            
+
+        </div>
+    )
+
+
+}
+
+export default filler;
