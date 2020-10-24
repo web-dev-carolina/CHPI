69de55fd95f5af48d5e144a48c983d60b4bcc516
Zhennan (Bill) Luo <zhennanl@live.unc.edu>
2020-10-23 22:51:55 -0400

    Add Testimonials page

--- chpi-website/src/Component/Testimonials.js
+++ chpi-website/src/Component/Testimonials.js
@@ -4,7 +4,15 @@ import '../CSS/Testimonials.css';
 function Testimonials(){
     return (
         <div className="testimonials">
-
+            <div className="wrapper">
+                <div className="title">
+                    <h1>Testimonials</h1>
+                </div>
+                <div className="content">
+                    <h4> A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
+                    In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.</h4>
+                </div>
+            </div>
         </div>
     )
 }
