12fa6e763a96c53f137843c1d59a0b01e7ae04c4
selenali07 <selenali@live.unc.edu>
2020-11-01 20:01:19 -0500

    made content cards for about homelessness page

--- chpi-website/src/Component/Homelessness.js
+++ chpi-website/src/Component/Homelessness.js
@@ -2,8 +2,29 @@ import React from 'react'
 import '../CSS/Homelessness.css';
 import { Container, Row, Col } from 'react-grid-system';
 import {  Link, animateScroll as scroll } from "react-scroll";
+import {Button,
+        Card,
+        CardActionArea,
+        CardActions,
+        CardContent,
+        CardMedia,
+        makeStyles,
+        Typography} from '@material-ui/core';
+import logo1 from '../Images/chapelHill.jpg'
+import logo2 from '../Images/hc.jpeg'
+import logo3 from '../Images/homelessnessInA.jpg'
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 180,
+    },
+  });
 
 export default function Homelessness(){
+  const classes = useStyles();
     const scrollToTop = () => {
         scroll.scrollToTop();
     };
@@ -12,56 +33,140 @@ export default function Homelessness(){
       };
     return (
         <div className="homelessness">
-            <div className = "wrapper">
                 <div className = "header">
+                    <div className = "headerContent">
                     <h1>About Homelessness</h1>
+                    <p>Learn more about the state of Homelessness</p>
+                    <Link
+                        activeClass="active"
+                        to="info"
+                        spy={true}
+                        smooth={true}
+                        offset={-100}
+                        duration={500}
+                        onSetActive={handleSetActive}>
+                    <Button size="medium" color="primary">
+                                Learn More
+                                </Button></Link>
+                    </div>
                 </div>
-                <Container>
-                    <Row className = "row">
-                        <Col className = "col">
-                            <h1>Origin of Mass Homelessness</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                            <Link
-                                activeClass="active"
-                                to="hoh"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                                onSetActive={handleSetActive}
-                            ><button>History of Homelessness</button></Link>
-                        </Col>
-                        <Col className = "col">
-                            <img src ="https://snworksceo.imgix.net/dth/90ff4c4b-0f1c-4e82-8903-0adc42c90587.sized-1000x1000.jpeg?w=1000" alt = "ch"></img>
-                            <h1>Local Effects?</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                            <Link
-                                activeClass="active"
-                                to="hich"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                                onSetActive={handleSetActive}
-                            ><button>Homelessness in Chapel Hill</button></Link>
-                        </Col>
-                        <Col className = "col">
-                            <h1>What are the solutions?</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                            <Link
-                                activeClass="active"
-                                to="eh"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                                onSetActive={handleSetActive}
-                            ><button>Ending Homelessness</button></Link>
+                <Container id = "info">
+                    <Row>
+                    <Link
+                        activeClass="active"
+                        to="hich"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        onSetActive={handleSetActive}>
+                    <Col>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.media}
+                                image={logo1}
+                                title="Homelessness in Chapel Hill"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                Homelessness in Chapel Hill
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                Learn More
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Col>
+                    </Link>
+                    <Link
+                            activeClass="active"
+                            to="hoh"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onSetActive={handleSetActive}>
+                    <Col>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.media}
+                                image={logo2}
+                                title="History of Homelessness"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                History of Homelessness
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                Learn More
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Col>
+                    </Link>
+                    <Col>
+                    <Link
+                            activeClass="active"
+                            to="eh"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onSetActive={handleSetActive}>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.media}
+                                image={logo3}
+                                title="Ending Homelessness"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                Ending Homelessness
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                Learn More
+                                </Button>
+                            </CardActions>
+                        </Card>
+                        </Link>
                         </Col>
                     </Row>
                 </Container>
-
                 <div className = "about-homelessness-content">
+                    <div id = "hich">
+                        <h1>Homelessness in Chapel Hill</h1>
+                        <h2>The Situation</h2>
+                        <p>In 2018, Orange County found that on the night of the Point in Time Homelessness count there were 152 homeless individuals in Orange County. These people were predominantly male, adults, and disproportionately black.  The number of people experiencing Chronic Homelessness, meaning they suffer from a disability and have been homeless for over 1 year, has been steadily decreasing since 2011. However, while nationally and statewide total homelessness is decreasing, the total number of people experiencing homelessness is actually increasing in Chapel Hill. Essentially, Chapel Hill is failing to prevent homelessness.</p>
+                        <h2>The Strategy</h2>
+                        <p>Orange County prioritizes housing for the most vulnerable, (meaning most likely to die). When individuals with the greatest need receive housing first, they are given a safe and stable environment in which they can dedicate the time and energy towards combatting mental health struggles and addiction as well as and generating an income. This strategy is based on years of research and has proven to be the most effective way to combat homelessness.</p>
+                        <h2>How To Help</h2>
+                        <p>If you would like to donate your time to combating homelessness in our county please explore the following sites:</p>
+                        <p>Orange County's Habitat for Humanity If you would like to donate to an organization making affordable housing more prevalent in Orange County we recommend you consider donating to chapter, or attending Mixed Concrete, an art sale that raises money for affordable housing in Orange County</p>
+                    </div>
                     <div id = "hoh">
                         <h1>History of Homelessness</h1>
                         <h2>The Origin of Mass Homelessness</h2>
@@ -96,16 +201,6 @@ export default function Homelessness(){
                         arrived at more effective strategies that together are called the housing first system.
                         </p>
                     </div>
-                    <div id = "hich">
-                        <h1>Homelessness in Chapel Hill</h1>
-                        <h2>The Situation</h2>
-                        <p>In 2018, Orange County found that on the night of the Point in Time Homelessness count there were 152 homeless individuals in Orange County. These people were predominantly male, adults, and disproportionately black.  The number of people experiencing Chronic Homelessness, meaning they suffer from a disability and have been homeless for over 1 year, has been steadily decreasing since 2011. However, while nationally and statewide total homelessness is decreasing, the total number of people experiencing homelessness is actually increasing in Chapel Hill. Essentially, Chapel Hill is failing to prevent homelessness.</p>
-                        <h2>The Strategy</h2>
-                        <p>Orange County prioritizes housing for the most vulnerable, (meaning most likely to die). When individuals with the greatest need receive housing first, they are given a safe and stable environment in which they can dedicate the time and energy towards combatting mental health struggles and addiction as well as and generating an income. This strategy is based on years of research and has proven to be the most effective way to combat homelessness.</p>
-                        <h2>How To Help</h2>
-                        <p>If you would like to donate your time to combating homelessness in our county please explore the following sites:</p>
-                        <p>Orange County's Habitat for Humanity If you would like to donate to an organization making affordable housing more prevalent in Orange County we recommend you consider donating to chapter, or attending Mixed Concrete, an art sale that raises money for affordable housing in Orange County</p>
-                    </div>
                     <div id = "eh">
                         <h1>Ending Homelessness</h1>
                         <h2>What does "ending homelessness" mean?</h2>
@@ -128,7 +223,7 @@ export default function Homelessness(){
                         <p>Housing First is the principle of prioritizing providing permanent housing and then after the person is housed providing the necessary services to address any other issues such as addiction, mental health treatment, and unemployment. When someone lacks permanent housing, survival is the only priority. Housing must come first, the rest will follow.</p>
                     </div>
                 </div>
-            </div>
+            
             <button onClick={scrollToTop}>scroll to top</button>
         </div>
     );
