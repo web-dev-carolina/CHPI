88086c705629588c922cae6afd8ebecf3087256d
minh100 <minh100@live.unc.edu>
2020-11-08 22:55:52 -0500

    testimonials responsive

--- chpi-website/src/Component/Testimonials.js
+++ chpi-website/src/Component/Testimonials.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import '../CSS/Testimonials.css';
 import Carousel from 'react-bootstrap/Carousel'
 
-function Testimonials(){
+function Testimonials() {
     return (
         <div className="testimonials">
             <div className="wrapper">
@@ -11,30 +11,30 @@ function Testimonials(){
                     <h1>Testimonials</h1>
                 </div>
                 <div className="content">
-                <Carousel>
-                    <Carousel.Item>
-                        A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
-                        In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
+                    <Carousel>
+                        <Carousel.Item>
+                            A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness.
+                            In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
                         <Carousel.Caption>
-                            <h3>- Non Anonymous Person</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
-                        In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
+                                <h3>- Non Anonymous Person</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness.
+                            In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
                         <Carousel.Caption>
-                            <h3>- Non Anonymous Person</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
-                        In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
+                                <h3>- Non Anonymous Person</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness.
+                            In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
                         <Carousel.Caption>
-                            <h3>- Non Anonymous Person</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-                </div> 
+                                <h3>- Non Anonymous Person</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
             </div>
         </div>
     );
