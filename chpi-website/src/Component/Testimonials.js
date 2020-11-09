eb644021f938e684b3c6d1fddf01811ae2a01480
Zhennan (Bill) Luo <zhennanl@live.unc.edu>
2020-11-08 22:21:21 -0500

    Add carousel to testimonials

--- chpi-website/src/Component/Testimonials.js
+++ chpi-website/src/Component/Testimonials.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import '../CSS/Testimonials.css';
+import Carousel from 'react-bootstrap/Carousel'
 
 function Testimonials(){
     return (
@@ -9,12 +11,33 @@ function Testimonials(){
                     <h1>Testimonials</h1>
                 </div>
                 <div className="content">
-                    <h4> A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
-                    In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.</h4>
-                </div>
+                <Carousel>
+                    <Carousel.Item>
+                        A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
+                        In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
+                        <Carousel.Caption>
+                            <h3>- Non Anonymous Person</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
+                        In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
+                        <Carousel.Caption>
+                            <h3>- Non Anonymous Person</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        A UNC student-founded platform to keep Chapel Hill's most vulnerable residents from becoming homeless and to educate the UNC community about the underlying causes of mass homelessness. 
+                        In Chapel Hill, we are the only source of diversion funding - the most immediate and research-backed means to prevent homelessness.
+                        <Carousel.Caption>
+                            <h3>- Non Anonymous Person</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                </div> 
             </div>
         </div>
-    )
+    );
 }
 
 export default Testimonials;
