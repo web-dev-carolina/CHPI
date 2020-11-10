734678733abda79aedd1ec623d89d1620b848190
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2020-11-09 22:33:22 -0500

    firebase integration

--- chpi-website/src/Component/Contact.js
+++ chpi-website/src/Component/Contact.js
@@ -3,13 +3,17 @@ import '../CSS/Contact.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap';
+import ScriptTag from 'react-script-tag';
 import '../App.css'
+import firebase from 'firebase';
 
 function Contact(){
     return (
+        <div>
+            {/* <ScriptTag type="text/javascript" src="../firebase.js" /> */}
         <div className="contact">
             <Form className="formcontainer">
-            <Form.Group controlId="formBasicName" class="fullname">
+            <Form.Group controlId="formBasicName" className="fullname">
                 <Form.Label>Full Name</Form.Label>
                 <Form.Control type="name" placeholder="Enter full name" />
             </Form.Group>
@@ -37,11 +41,44 @@ function Contact(){
                 <Form.Label>Message</Form.Label>
                 <Form.Control as="textarea" rows={5} />
             </Form.Group>
-            <Button class="secondary-color" type="Message">
+            <Button id="contact-submit" className="secondary-color" >
                 Submit
             </Button>
             </Form>
         </div>
+        </div>
+
+        
+        
     )
 }
+//do not change
+var firebaseConfig = {
+    apiKey: "AIzaSyDBuOlw9NFZ5-P6uEJMRnc5nIR6SC3O8EY",
+    authDomain: "chpi-contact.firebaseapp.com",
+    databaseURL: "https://chpi-contact.firebaseio.com",
+    projectId: "chpi-contact",
+    storageBucket: "chpi-contact.appspot.com",
+    messagingSenderId: "417414018305",
+    appId: "1:417414018305:web:0c72d7e428a90a4ecf5700"
+  };
+  firebase.initializeApp(firebaseConfig);
+  // Write to databse
+  function firebasePush(name, email, subject, message) {
+    // Ensures firebase is initialized 
+    if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+    }
+    // adds to contact dataset 
+    firebase.database().ref('contact').push().set(
+    {
+        name,
+        email,
+        subject,
+        message,
+    });
+  }
+  //TODO: addEvent listener to submit, with handleSubmit fn
+  // handle submit fn should get name,email,subject,message through document.getElementById.innerHTML
+  // pass these values into firebasePush()
 export default Contact;
